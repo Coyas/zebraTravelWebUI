8e63f7b7291b2ffe94644957e9127e7a74dbcda5
Ailton Duarte <adidas.coyas@gmail.com>
2020-05-05 02:16:11 -0100

    dinamicar divisor content

--- components/Divisor/index.js
+++ components/Divisor/index.js
@@ -1,7 +1,19 @@
 import Link from "next/link";
 import divi from "./divisor.module.scss";
 import Hr from "../Hr";
+import Voltar from "../Voltar";
+
 const Divisor = (props) => {
+    let Qual;
+    console.log("props: " + props.voltar);
+
+    if (props.voltar === "true") {
+        Qual = <Voltar />;
+        console.log("true");
+    } else {
+        Qual = <Hr height="2" opacidade="1" cor={props.cores} />;
+        console.log("false");
+    }
     return (
         <>
             <div className={"container " + divi.boxi}>
@@ -15,13 +27,11 @@ const Divisor = (props) => {
                                 <span>
                                     <img src="/img/flexa.svg" />
                                 </span>
-                                SOBRE NÓS
+                                {props.sobre}
                             </a>
                         </p>
                     </div>
-                    <div class="column">
-                        <Hr height="2" opacidade="1" cor={props.cores} />
-                    </div>
+                    <div class="column">{Qual}</div>
                 </div>
             </div>
         </>
