9bdf7a2c0da406f3f58e293f32d55f2a78981456
Ailton Duarte <adidas.coyas@gmail.com>
2020-05-05 02:18:26 -0100

    criar o componente asidebar, filtro de eperiencia

---
+++ components/Explorebox/index.js
@@ -0,0 +1,74 @@
+import excss from "./index.module.scss";
+import Link from "next/link";
+
+const Explorebox = () => {
+    return (
+        <>
+            <aside className={"menu " + excss.box}>
+                <p className="menu-label">Experiências</p>
+                <ul className="menu-list">
+                    <li>
+                        <Link href="">
+                            <a>Featured Tours and Tickets (5)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Cruises, Sailing & Water Tours (1)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Cultural & Theme Tours (2)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Day Trips & Excursions (1)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Outdoor Activities (3)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Tours & Sightseeing (4)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>Walking & Biking Tours (2)</a>
+                        </Link>
+                    </li>
+                </ul>
+                <p className="menu-label">Duration</p>
+                <ul className="menu-list">
+                    <li>
+                        <Link href="">
+                            <a>1 to 4 hours (1)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>4 hours to 1 day (4)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>1 to 3 days (1)</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>3+ days (1)</a>
+                        </Link>
+                    </li>
+                </ul>
+            </aside>
+        </>
+    );
+};
+
+export default Explorebox;
