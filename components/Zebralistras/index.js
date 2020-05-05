c3ad7151d08d8782aacbf3eee7efb9ef16199653
Ailton Duarte <adidas.coyas@gmail.com>
2020-05-05 02:20:15 -0100

    criar componente zebraListras

---
+++ components/Zebralistras/index.js
@@ -0,0 +1,11 @@
+import zcss from "./index.module.scss";
+const Zebralistras = () => {
+    return (
+        <>
+            <figure className={"image " + zcss.listras}>
+                <img src="/img/ZebraListra.svg" />
+            </figure>
+        </>
+    );
+};
+export default Zebralistras;
