42c3dc743ffc6e9c4930a998f94a2d349cc5c520
Ailton Duarte <adidas.coyas@gmail.com>
2020-06-03 19:24:13 -0100

    mobile in contactos done

--- pages/contacto.js
+++ pages/contacto.js
@@ -36,7 +36,7 @@ const Contacto = () => {
                     </h2>
                 </section>
                 <section className={"container " + css.map}>
-                    <div className="columns">
+                    <div className="columns is-desktop">
                         <div className="column">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15422.962103562264!2d-24.4987502!3d14.8957806!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x35a681b0dafcbcba!2sZebra%20Travel%20Viagens%20%26%20Turismo!5e0!3m2!1spt-PT!2scv!4v1588503306490!5m2!1spt-PT!2scv"></iframe>
                         </div>
