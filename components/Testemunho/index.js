e27d26cbdb9224dc7a62bf9e90dcc347fdb60d12
Ailton Duarte <adidas.coyas@gmail.com>
2020-06-25 14:30:45 -0100

    add testimunhos request api

--- components/Testemunho/index.js
+++ components/Testemunho/index.js
@@ -1,28 +1,53 @@
 import css from "./index.module.scss";
+import api from "../../lib/api";
 // https://bulma.io/images/placeholders/128x128.png
 const Testemunho = () => {
+    const { response, error, isLoading } = api("/api/testimunhos");
+
+    console.log("response");
+    console.log(response);
+
+    // {
+    //     !isLoading &&
+    //         response.map((teste) => (
+    //             <li key={teste.id}>
+    //                 <p>{teste.nome}</p>
+    //                 <p>{teste.message}</p>
+    //                 <img src={teste.url} alt={teste.nome} />
+    //             </li>
+    //         ));
+    // }
+
     return (
         <>
             <div className="columns">
-                <div className={"column " + css.center}>
-                    <article className={css.article}>
-                        <figure className={"image is-128x128 " + css.display}>
-                            <img className="is-rounded" src="/img/t2.png" />
-                        </figure>
-                        <p>
-                            <span className="icon">
-                                <i className="fas fa-quote-left"></i>
-                            </span>
-                        </p>
-                        <p className={css.txt}>
-                            "Colonial Guest House was lovely as were the staff.
-                            A nice place to stay. Our day tour of the v olcanic
-                            crater was excellent. Top marks.”
-                        </p>
-                        <p className={css.autor}>Mr. Alker - Switzerland</p>
-                    </article>
-                </div>
-                <div className={"column " + css.center}>
+                {!isLoading &&
+                    response.map((dados) => (
+                        <div className={"column " + css.center}>
+                            <article className={css.article}>
+                                <figure
+                                    className={
+                                        "image is-128x128 " + css.display
+                                    }
+                                >
+                                    <img
+                                        className="is-rounded"
+                                        src={`${process.env.API_BASE_URL}${dados.image.url}`}
+                                    />
+                                </figure>
+                                <p>
+                                    <span className="icon">
+                                        <i className="fas fa-quote-left"></i>
+                                    </span>
+                                </p>
+                                <p className={css.txt}>"{dados.message}”</p>
+                                <p className={css.autor}>
+                                    {dados.nome} - {dados.pais}
+                                </p>
+                            </article>
+                        </div>
+                    ))}
+                {/* <div className={"column " + css.center}>
                     <article className={css.article}>
                         <figure className={"image is-128x128 " + css.display}>
                             <img className="is-rounded" src="/img/t1.png" />
@@ -40,10 +65,23 @@ const Testemunho = () => {
                         </p>
                         <p className={css.autor}>Ms. Marleen - Belgium</p>
                     </article>
-                </div>
+                </div> */}
             </div>
         </>
     );
 };
+
+// export async function getStaticProps() {
+//     const { response, error, isLoading } = api("/api/testimunhos");
+//     return {
+//         props: { response } // will be passed to the page component as props
+//     };
+// }
+
+// Testemunho.getInitialProps = async (ctx) => {
+//     const res = await fetch("/api/testimunhos");
+//     const json = await res.json();
+//     return { stars: json, agua: "agua terra" };
+// };
 
 export default Testemunho;
