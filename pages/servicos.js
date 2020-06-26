36f1f9741bed00a8a037e630e92c7a7ac32a2892
Ailton Duarte <adidas.coyas@gmail.com>
2020-06-26 12:15:20 -0100

    add imgem to service api

--- pages/servicos.js
+++ pages/servicos.js
@@ -4,9 +4,21 @@ import Headlogo from "../components/Headlogo";
 import { withTranslation } from "../i18n";
 import { useFetchUser } from "../lib/user";
 import Head from "next/head";
+import showdown from "showdown";
 
-const Servicos = ({ t }) => {
+const Servicos = ({ t, dados }) => {
     const { user, loading } = useFetchUser();
+    console.log("dados");
+    console.log(dados);
+    // const res = dados.content.replace(/\n/g, "<br/>");
+
+    const createMarkup = () => {
+        const converter = new showdown.Converter();
+        const html = converter.makeHtml(dados?.content);
+        // console.log(html);
+        return { __html: html };
+    };
+
     return (
         <>
             <Layout user={user}>
@@ -56,58 +68,16 @@ const Servicos = ({ t }) => {
                 <section className={"container " + sescss.detalhes}>
                     <div className="columns is-desktop">
                         <div className={"column " + sescss.arti}>
-                            <article>
-                                Itinerary
-                                <br />
-                                This is a typical itinerary for this product
-                                <br />
-                                <br />
-                                Stop At: Parque Natural do Fogo, Fogo Island,
-                                Fogo Cape Verde
-                                <br />
-                                <br />
-                                Departure from São Filipe then stopping at
-                                Forno, where we visit one of the areas where the
-                                drop-by-drop irrigation method is used (a
-                                technique adopted by farms on the island in
-                                order to minimize the effect of drought). We
-                                then continue to Achada Furna, a chance to see
-                                the communities where new houses were built
-                                after the eruption of 1995 and 2014 for the
-                                population of Chã das Caldeiras. Many of the
-                                locals have now returned to the Caldeira.
-                                Continuing on, we stop in Cabeça Fundão where we
-                                can see the old craters and the wonderful mixed
-                                colors in the landscape. Finally, we arrive at
-                                the entrance of the National Park of Chã das
-                                Caldeiras.
-                                <br />
-                                <br />
-                                Here you get a stupendous view from the highest
-                                point from Cape Verde and get the opportunity to
-                                trek to the 2014 crater (Optional). You can find
-                                places with extreme hot spots from the Sulfur. A
-                                multi-colored landscape well worth a visit. The
-                                trekking takes approximately 3 hours depending
-                                on your shape. We visited one of the two
-                                destroyed villages in the eruption of 2015.
-                                Almost 98% of the houses, schools, restaurants,
-                                hotels were decimated but people are coming
-                                back, keen to rebuild everything again.
-                                <br />
-                                <br />
-                                Lunch at Marisa house followed by wine tasting
-                                the traditional Chã das Caldeiras wine better
-                                known as Manecon. You also get the chance to buy
-                                one of the best coffees in the
-                            </article>
+                            <article dangerouslySetInnerHTML={createMarkup()} />
                         </div>
                         <div className="column">
                             <figure
                                 className="image"
-                                // style={{ width: "105.6%" }}
+                                style={{ width: "105.7%" }}
                             >
-                                <img src="/img/b.png" />
+                                <img
+                                    src={`${process.env.API_BASE_URL}${dados.imagem.url}`}
+                                />
                             </figure>
                         </div>
                     </div>
@@ -117,10 +87,34 @@ const Servicos = ({ t }) => {
     );
 };
 
-Servicos.getInitialProps = async () => {
-    const obj = { namespacesRequired: ["servico", "footer", "navbar"] };
+// Servicos.getInitialProps = (ctx) => {
+//     const obj = {
+//         namespacesRequired: ["servico", "footer", "navbar"]
+//     };
 
-    return obj;
-};
+//     return obj;
+// };
+
+export async function getServerSideProps(context) {
+    const url = `${process.env.API_BASE_URL}/service-text`;
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    // console.log("api response");
+    // console.log(response);
+    const dados = await response.json();
+
+    const obj = {
+        namespacesRequired: ["servico", "footer", "navbar"]
+    };
+
+    return {
+        props: { dados, obj }
+    };
+}
 
 export default withTranslation("servico")(Servicos);
