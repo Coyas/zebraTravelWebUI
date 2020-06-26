5d748012e4b35a3807cc82468e77996aefb3cbc4
Ailton Duarte <adidas.coyas@gmail.com>
2020-06-26 13:27:32 -0100

    create link api

---
+++ pages/api/links.js
@@ -0,0 +1,26 @@
+import fetch from "isomorphic-unfetch";
+
+export default async function links(req, res) {
+    try {
+        const url = `${process.env.API_BASE_URL}/links`;
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        console.log("api response");
+        console.log(response);
+        const dados = await response.json();
+        console.log("dados links api");
+        console.log(dados);
+
+        return res.status(200).json(dados);
+    } catch (error) {
+        return res.status(error.status || 500).json({
+            code: error.code,
+            error: error.message
+        });
+    }
+}
