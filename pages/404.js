1996e075da059409cdb5e706ece1561fa60e15dd
Ailton Duarte <adidas.coyas@gmail.com>
2021-05-08 10:03:15 -0100

    translate 404 name

--- pages/404.js
+++ pages/404.js
@@ -22,7 +22,10 @@ const NotFound404 = () => {
                 <div className={"container " + notcss.fornfor}>
                     <div className="columns">
                         <div className="column is-half is-offset-one-quarter">
-                            <h1 className={notcss.tite}>404</h1>
+                            <h1 className={notcss.tite}>{t("404")}</h1>
+                        </div>
+                        <div className="column is-half is-offset-one-quarter">
+                            <h6>404</h6>
                         </div>
                     </div>
                 </div>
