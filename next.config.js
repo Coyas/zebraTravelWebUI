343f8ac8b6860f8298e1645efa59713eaf7984a1
Ailton Duarte <adidas.coyas@gmail.com>
2021-06-23 12:58:33 -0100

    fix api_base url from localhost:8337 to dns name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     env: {
-        API_BASE_URL: "http://localhost:8337",
+        API_BASE_URL: "https://dash.zebratravel.net",
         SiteKey: "6LfQgq8ZAAAAAFV14LeRqM4Wl7n4rOUDi9r-cbG9",
         SecretKey: "6LfQgq8ZAAAAABMPHcpxSr4dVr-yWP-ZlWawhsQF"
     }
