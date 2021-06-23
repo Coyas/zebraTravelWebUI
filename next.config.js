fbe8f09bcdf151f3ddd436e3b43fe74468c7b2d2
Ailton Duarte <adidas.coyas@gmail.com>
2021-06-23 12:41:04 -0100

    change api port

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     env: {
-        API_BASE_URL: "http://localhost:8333",
+        API_BASE_URL: "http://localhost:8337",
         SiteKey: "6LfQgq8ZAAAAAFV14LeRqM4Wl7n4rOUDi9r-cbG9",
         SecretKey: "6LfQgq8ZAAAAABMPHcpxSr4dVr-yWP-ZlWawhsQF"
     }
