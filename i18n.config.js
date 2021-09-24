08c4bd63627c620c701f0d825d22e4472350a9b3
Ailton Duarte <adidas.coyas@gmail.com>
2021-09-24 11:32:07 -0100

    add the new i18n config

---
+++ i18n.config.js
@@ -0,0 +1,20 @@
+// const NextI18Next = require("next-i18next/dist/commonjs");
+// import NextI18Next from "next-i18next/dist/commonjs";
+//const NextI18Next = require("next-i18next").default;
+/*
+module.exports = new NextI18Next({
+    defaultLanguage: "pt",
+    otherLanguages: ["en", "fr"]
+    // localeSubpaths: {
+    //     pt: "pt",
+    //     en: "en",
+    //     fr: "fr"
+    // }
+});
+*/
+module.exports = {
+    i18n: {
+        defaultLocale: "pt",
+        locales: ["en", "fr"]
+    }
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { i18n } = require("./i18n.config");
 
 module.exports = {
     env: {
@@ -6,5 +7,6 @@ module.exports = {
         API_BASE_URL: "http://localhost:8333",
         SiteKey: "6LfQgq8ZAAAAAFV14LeRqM4Wl7n4rOUDi9r-cbG9",
         SecretKey: "6LfQgq8ZAAAAABMPHcpxSr4dVr-yWP-ZlWawhsQF"
-    }
+    },
+    i18n
 };
