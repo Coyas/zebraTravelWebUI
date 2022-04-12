7adeec6dccb76711bba3faee702c313e1d699219
Ailton Duarte <adidas.coyas@gmail.com>
2022-04-12 13:20:07 -0100

    fix for strapi v4

--- pages/servicos.js
+++ pages/servicos.js
@@ -4,55 +4,107 @@ import Headlogo from "../components/Headlogo";
 import { useFetchUser } from "../lib/user";
 import Head from "next/head";
 import showdown from "showdown";
-import { useEffect } from "react";
-import api from "../lib/api";
+// import { useEffect } from "react";
+// import api from "../lib/api";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import MyCarousel from "../components/MyCarousel";
+const qs = require("qs");
 
-const Servicos = ({ dados }) => {
+const Servicos = ({ servico, servicetext }) => {
+    const servicoarray = servico.data; // make the services dada json
+    //dados2.data.attributes.imagem.data.attributes.url
+    const response = servicetext.data; // make the servicetext data json
+    const imagina = response.attributes.imagem?.data.attributes.url; // imagem estatica do servicetext
     const { t, i18n } = useTranslation("servico");
     const { user, loading } = useFetchUser();
-    const { response, isLoading } = api("/api/service");
+    // const { response, isLoading } = api("/api/service");
+    // console.log("testes de dados ");
+    // console.log(servicoarray);
 
     let data;
     let title = [];
     let images = [];
+
+    servicoarray.map((value, index) => {
+        images[index] = {
+            id: index,
+            title: value.attributes.image_pt.data.attributes.hash,
+            url: value.attributes.image_pt.data.attributes.url
+        };
+    });
+    // console.log("images");
+    // console.log(images);
+
+    // return null;
     switch (i18n.language) {
         case "pt": {
-            data = response?.content_pt;
-            dados.forEach((item) => {
-                title.push(item.servico_pt);
-            });
-            dados.forEach((item) => {
-                images.push(item.image_pt);
+            data = response.attributes?.content_pt;
+            // servicoarray.forEach((item) => {
+            //     title.push(item.attributes.servico_pt);
+            //     images.push(item.attributes.image_pt?.data?.attributes.url);
+            //     // console.log("item.attributes.image_pt  sdfdsddd");
+            //     // console.log(item.attributes.image_pt?.data?.attributes.url);
+            // });
+            // console.log(data);
+            // console.log(servicoarray);
+            // dada2.forEach((item) => {
+            //     images.push(item.image_pt);
+            // });
+            servicoarray.map((value, index) => {
+                title.push(value.attributes.servico_pt);
+                images[index] = {
+                    id: index,
+                    title: value.attributes.image_pt.data.attributes.hash,
+                    url: value.attributes.image_pt.data.attributes.url
+                };
             });
             break;
         }
         case "en": {
-            data = response?.content_en;
-            dados.forEach((item) => {
-                title.push(item.servico_en);
-            });
-            dados.forEach((item) => {
-                images.push(item.image_en);
+            data = response.attributes?.content_en;
+            // servicoarray.attributes.forEach((item) => {
+            //     title.push(item.attributes.servico_en);
+            //     images.push(item.attributes.image_en?.data?.attributes.url);
+            // });
+            // dados.forEach((item) => {
+            //     images.push(item.image_en);
+            // });
+            servicoarray.map((value, index) => {
+                title.push(value.attributes.servico_en);
+                images[index] = {
+                    id: index,
+                    title: value.attributes.image_en.data.attributes.hash,
+                    url: value.attributes.image_en.data.attributes.url
+                };
             });
             break;
         }
         case "fr": {
-            data = response?.content_fr;
-            dados.forEach((item) => {
-                title.push(item.servico_fr);
-            });
-            dados.forEach((item) => {
-                images.push(item.image_fr);
+            data = response.attributes?.content_fr;
+            // servicoarray.attributes.forEach((item) => {
+            //     title.push(item.attributes.servico_fr);
+            //     images.push(item.attributes.image_fr?.data?.attributes.url);
+            // });
+            // dados.forEach((item) => {
+            //     images.push(item.image_fr);
+            // });
+            servicoarray.map((value, index) => {
+                title.push(value.attributes.servico_fr);
+                images[index] = {
+                    id: index,
+                    title: value.attributes.image_fr.data.attributes.hash,
+                    url: value.attributes.image_fr.data.attributes.url
+                };
             });
             break;
         }
     }
 
-    //console.log("data:");
-    // console.log("ulrl");
+    // console.log("title");
+    // console.log(title);
+
+    // return null;
 
     const createMarkup = () => {
         const converter = new showdown.Converter();
@@ -121,7 +173,7 @@ const Servicos = ({ dados }) => {
                             >
                                 <img
                                     style={{ height: "100vh" }}
-                                    src={`${process.env.API_BASE_URL}${response?.imagem?.url}`}
+                                    src={`${imagina}`}
                                 />
                             </figure>
                         </div>
@@ -164,7 +216,29 @@ const Servicos = ({ dados }) => {
 // }
 
 export const getStaticProps = async ({ locale }) => {
-    const url = `${process.env.API_BASE_URL}/servicos`;
+    const query = qs.stringify(
+        {
+            // sort: ["title:asc"],
+            // filters: {
+            //     title: {
+            //         $eq: "hello"
+            //     }
+            // },
+            populate: "*"
+            // fields: ["imagem", "content_pt", "content_en", "content_fr"]
+            // pagination: {
+            //     pageSize: 10,
+            //     page: 1
+            // },
+            // publicationState: "live",
+            // locale: ["en"]
+        },
+        {
+            encodeValuesOnly: true // prettify url
+        }
+    );
+
+    const url = `${process.env.API_BASE_URL}/servicos?${query}`;
 
     const response = await fetch(url, {
         method: "GET",
@@ -173,9 +247,25 @@ export const getStaticProps = async ({ locale }) => {
         }
     });
 
-    // console.log("api response");
+    // console.log("service response before await");
     // console.log(response);
-    const dados = await response.json();
+    const servico = await response.json();
+    // console.log("service response after await");
+    // console.log(dados);
+    const url2 = `${process.env.API_BASE_URL}/servicetext?${query}`;
+    const response2 = await fetch(url2, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    // console.log("query qs");
+    // console.log(url2);
+
+    const servicetext = await response2.json();
+    // console.log("api servicetext response");
+    // console.log(dados2.data.attributes.imagem.data.attributes.url);
 
     return {
         props: {
@@ -184,7 +274,8 @@ export const getStaticProps = async ({ locale }) => {
                 "footer",
                 "navbar"
             ])),
-            dados
+            servico,
+            servicetext
         } // will be passed to the page component as props
     };
 };
