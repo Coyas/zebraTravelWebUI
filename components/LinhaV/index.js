07e2572ba36cc9f99562d9bfc569d6ad440002c3
Ailton Duarte <adidas.coyas@gmail.com>
2022-05-18 11:37:08 -0100

    add swr datafetcing to linhaV

--- components/LinhaV/index.js
+++ components/LinhaV/index.js
@@ -1,19 +1,27 @@
-import api from "../../lib/api";
+import fetcher from "../../lib/fetcher";
+import useSWR from "swr";
 import indexcss from "../../pages/styles/index.module.scss";
 
 const LinhaV = () => {
-    const { response, error, isLoading } = api("/api/links");
+    const { data, error } = useSWR(
+        `${process.env.API_BASE_URL}/links-social`,
+        fetcher
+    );
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
     return (
         <div className={indexcss.colunaIcon}>
             <div className={indexcss.linha1}></div>
             <div className={indexcss.Ticons}>
-                <a href={response?.facebook} target="_blank">
+                <a href={data.data.attributes.facebook} target="_blank">
                     <i className="fab fa-facebook-f"></i>
                 </a>
-                <a href={response?.instagram} target="_blank">
+                <a href={data.data.attributes.instagram} target="_blank">
                     <i className="fab fa-instagram"></i>
                 </a>
-                <a href={response?.youtube} target="_blank">
+                <a href={data.data.attributes.youtube} target="_blank">
                     <i className="fab fa-youtube"></i>
                 </a>
             </div>
